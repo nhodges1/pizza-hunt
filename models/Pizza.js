e1ed640e52130c2d3f08394ef7acf8d43f74b0ef
nhodges <hodges27@outlook.com>
2022-06-11 17:27:38 -0400

    update

---
+++ models/Pizza.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+// create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    topppings: []
+});
+
+// export the Pizza model
+module.exports = Pizza;
